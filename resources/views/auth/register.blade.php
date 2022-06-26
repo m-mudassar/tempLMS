ec2c145df20adbde26701c8484b68eceb736b01c
Mudassar Malik <codingbiters@gmail.com>
2022-06-26 20:20:03 +0500

    first commit

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.master_authentication')
+
+{{-- Only Bootstrap 5.1.x is supported --}}
+
+@section('form')
+<h1>Basic registration setup</h1>
+@endsection
---
+++ resources/views/layouts/master_authentication.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+
+{{-- bootstrap version 5.1.x is supported in this application --}}
+{{-- using higher or lower version than this may cause the application to crash --}}
+
+    <div class="container-fluid vh-100">
+        @yield('form')
+    </div>
+</body>
+</html>
